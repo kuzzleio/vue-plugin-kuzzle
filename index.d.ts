b6975cbc4eb9b956e33163bc99c23f707d08fc1b
Luca Marchesini <xbill82@gmail.com>
2019-08-28 17:26:21 +0200

    [typescript] Added definitions filet

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare module 'vue-kuzzle' {
+  import Vue, { PluginFunction } from 'vue';
+  export const install: PluginFunction<{}>;
+
+  module 'vue/types/vue' {
+    interface Vue {
+      $kuzzle: any;
+    }
+  }
+}
+
